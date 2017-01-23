cffd821c4fa825143202e3d5e6c911c6c7fbf6df
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2017-01-23 11:43:25 +0100

    linting

--- client/main.js
+++ client/main.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
-import { install } from 'electron-react-devtools';
 import App from './components/app.jsx';
 import Dashboard from './views/dashboard.jsx';
 import Invoices from './views/invoices.jsx';
 import Clients from './views/clients.jsx';
 
+import { install } from 'electron-react-devtools';
 install();
 
 render((
--- server/revenue.js
+++ server/revenue.js
@@ -1,24 +1,24 @@
 const revenue = require('revenue-core');
 
-
-
 let configurator = (db) => {
   let config = db.get('config').value();
   return config;
-}
+};
 
 let getData = (name) => {
   let cache = {};
 
   return new Promise((ok, fail) => {
-    if (cache[name]) return ok(cache[name]);
+    if (cache[name]) {
+      return ok(cache[name]);
+    }
     revenue({}, configurator, (db, lastUpdate, options) => {
       let data = db.get(name).value();
       cache[name] = data;
       ok(cache[name]);
     });
-  })
-}
+  });
+};
 
 module.exports = {
   getData: getData,
